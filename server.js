229d12fb0ba5e2188ba119dce3116026c244e9dd
yuvalbl <yuval.bar.levi@gmail.com>
2018-07-01 18:14:17 +0300

    basic api

--- server.js
+++ server.js
@@ -2,7 +2,40 @@ const db = require('./db');
 const express = require('express');
 const app = express();
 
-app.get('/about', (req, res) => res.send('About page'));
+app.use(express.json());
+
+app.get('/about', (req, res) => res.send(`
+  <!DOCTYPE html>
+  <html>
+  <head></head>
+  <body>
+    <button onclick="test()">Test me</button>
+    <script>
+      function test() {
+        putData('/posts/10', {
+          userId: 42,
+          title: 'who is moshe?'
+        })
+        .then(data => console.log(data)) // JSON from \`response.json()\` call
+        .catch(error => console.error(error))
+      }
+      
+      function putData(url, data) {
+        // Default options are marked with *
+        return fetch(url, {
+          body: JSON.stringify(data), // must match 'Content-Type' header
+          headers: {
+            'content-type': 'application/json'
+          },
+          method: 'PUT',  // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors'    // no-cors, cors, *same-origin
+        })
+        .then(response => response.json()) // parses response to JSON
+      }
+    </script>  
+  </body>
+  </html>
+`));
 
 app.get('/', (req, res) => {
   res.redirect('/posts');
@@ -15,6 +48,24 @@ app.get('/posts/', (req, res) => {
 app.get('/posts/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const post = db.find(post => post.id === id);
+  res.send(JSON.stringify(post));
+});
+
+
+app.put('/posts/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  let post = db.find(post => post.id === id);
+  if (post) {
+
+  } else {
+    post = {
+      id,
+      userId: req.body.userId,
+      title: req.body.title,
+      body: req.body.body
+    };
+    db.push(post);
+  }
   res.send(JSON.stringify(post));
 });
 
